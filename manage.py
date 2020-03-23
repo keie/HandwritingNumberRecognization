8f46a18ed7c47770b6e673568701244def1c5521
keie <hombrehealth@hotmail.com>
2020-03-23 19:14:12 -0400

    neural model created

--- manage.py
+++ manage.py
@@ -4,6 +4,25 @@ import matplotlib.pyplot as plt
 mnist = tf.keras.datasets.mnist #28x28 images of handw-written digits0-9
 
 (x_train,y_train),(x_test,y_test) = mnist.load_data()
-plt.imshow(x_train[0])
+x_train=tf.keras.utils.normalize(x_train,axis=1)
+x_test=tf.keras.utils.normalize(x_test,axis=1)
+
+
+#MODEL CREATION####################################
+model=tf.keras.models.Secuential()
+model.add(tf.keras.layers.Flatten())#inputLayer
+model.add(tf.keras.layers.Dense(128,activation=tf.nn.relu))#dense layer
+model.add(tf.keras.layers.Dense(128,activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(10,activation=tf.nn.softmax))#outputLayer
+#MODEL CREATION####################################
+
+##TRAINING MODEL#####################################
+model.compile(
+    optimizer='adam',
+    loss='sparse_categorical_crossentropy',
+
+)
+##TRAINING MODEL#####################################
+plt.imshow(x_train[0],cmap=plt.cm.binary)
 plt.show()
 #print(x_train[0])
